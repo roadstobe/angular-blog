f35062f358045a7ff541f25a4f5e0a7ad01ddfa5
roadstobe <roadstobe@gmail.com>
2020-05-06 14:34:39 +0300

    'createTemplate'

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  form: FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.minLength(6), Validators.required])
+    })
   }
 
+  submit() {
+    if(this.form.invalid){
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+  }
 }
