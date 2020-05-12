2de7e3dbb4e1cf1412a63279d5bae67ee6e5e793
roadstobe <roadstobe@gmail.com>
2020-05-12 11:50:42 +0300

    'ready'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule, Provider} from '@angular/core';
+import {registerLocaleData} from "@angular/common";
+import ukrLocale from "@angular/common/locales/uk";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,9 @@ import { PostComponent } from './shared/components/post/post.component';
 import {SharedModule} from "./shared/shared.module";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./shared/auth.interceptor";
+
+
+registerLocaleData(ukrLocale, 'ukr')
 
 const INTERSEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
